4f7f0e2e110acd99b025c49db5686e484454b7c9
furkan <furkanaldemir@gmail.com>
2019-05-03 22:33:52 +0300

    Update RGB sizes

--- fakequidditch.v
+++ fakequidditch.v
@@ -1,45 +1,45 @@
 module fakequidditch (clk, team1_vu_button, team1_vd_button, team2_vu_button, team2_vd_button, red, green, blue, hor_sync, ver_sync);
-	
+
 	input clk;
 	input team1_vu_button;
 	input team1_vd_button;
 	input team2_vu_button;
 	input team2_vd_button;
-	
+
 	wire clk_en;
 	parameter div = 2; // <-- divide 50 MHz by this
-	
+
 	wire move_down;
 	reg  next_line;
-	
+
 	wire [9:0]  instant_line;
 	wire [9:0] instant_pixel;
 	reg [9:0]  current_line;
 	reg [9:0] current_pixel;
-	
+
 	wire [9:0] team1_ver_position;
 	wire [9:0] team2_ver_position;
 	reg [9:0] team1_ver_pos;
 	reg [9:0] team2_ver_pos;
-	
+
 	output wire  hor_sync;
 	output wire  ver_sync;
-	output wire [3:0]   red;
-	output wire [3:0] green;
-	output wire [3:0]  blue;
-	
+	output wire [7:0]   red;
+	output wire [7:0] green;
+	output wire [7:0]  blue;
+
 	clock_divider cd (clk, div, clk_en);
-	
+
 	vga_horizontal vga_hor (clk_en, move_down, instant_pixel);
-	
+
 	vga_vertical vga_ver (clk_en, next_line, instant_line);
-	
+
 	team1_controller t1_ctrl (clk_en, team1_vu_button, team1_vd_button, team1_ver_position);
-	
+
 	team2_controller t2_ctrl (clk_en, team2_vu_button, team2_vd_button, team2_ver_position);
-	
+
 	vga_controller vga_cont (current_line, current_pixel, team1_ver_pos, team2_ver_pos, hor_sync, ver_sync, red, green, blue);
-	
+
 	always begin
 		next_line     <= move_down;
 		current_line  <= instant_line;
@@ -47,5 +47,5 @@ module fakequidditch (clk, team1_vu_button, team1_vd_button, team2_vu_button, te
 		team1_ver_pos <= team1_ver_position;
 		team2_ver_pos <= team2_ver_position;
 	end
-	
+
 endmodule
--- vga_controller/vga_controller.v
+++ vga_controller/vga_controller.v
@@ -3,30 +3,30 @@ module vga_controller(
 	input wire [9:0] current_pixel,
 	input wire [9:0] team1_ver_pos,
 	input wire [9:0] team2_ver_pos,
-	
+
 	output hor_sync,
 	output ver_sync,
-	output reg [3:0] red,
-	output reg [3:0] green,
-	output reg [3:0] blue
+	output reg [7:0] red,
+	output reg [7:0] green,
+	output reg [7:0] blue
 );
 
 	assign hor_sync = (current_pixel > 95) ? 1'b1 : 1'b0;
 	assign ver_sync = (current_line > 1)  ? 1'b1 : 1'b0;
 
-	
+
 	always begin
 		if (current_pixel < 784 && current_pixel > 143 && current_line < 515 && current_line > 34) begin
-			if (((current_line - team1_ver_pos)**2) + ((current_pixel - 50)**2) > 175)  red = 4'hF; 
-			else  red = 4'h0;
-			if (((current_line - team1_ver_pos)**2) + ((current_pixel - 50)**2) > 175 && ((current_line - team2_ver_pos)**2) + ((current_pixel - 590)**2) > 175) green = 4'hF;
-			else green = 4'h0;
-			if (((current_line - team2_ver_pos)**2) + ((current_pixel - 590)**2) > 175)  blue = 4'hF;
-			else blue = 4'h0;
+			if (((current_line - team1_ver_pos)**2) + ((current_pixel - 50)**2) > 175)  red = 8'b11111111;
+			else  red = 8'b0;
+			if (((current_line - team1_ver_pos)**2) + ((current_pixel - 50)**2) > 175 && ((current_line - team2_ver_pos)**2) + ((current_pixel - 590)**2) > 175) green = 8'b11111111;
+			else green = 8'b0;
+			if (((current_line - team2_ver_pos)**2) + ((current_pixel - 590)**2) > 175)  blue = 8'b11111111;
+			else blue = 8'b0;
 		end else begin
-			red   = 4'h0;
-			green = 4'h0;
-			blue  = 4'h0;
+			red   = 8'b0;
+			green = 8'b0;
+			blue  = 8'b0;
 		end
 	end
 endmodule
