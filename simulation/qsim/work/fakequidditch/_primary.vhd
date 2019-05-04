3a9dcda50b6cc9bd6c14efb0a2682ba30ddb8db1
caslanturk <50174398+caslanturk@users.noreply.github.com>
2019-05-04 09:48:15 +0300

    Attempt FPGA for the first time

--- simulation/qsim/work/fakequidditch/_primary.vhd
+++ simulation/qsim/work/fakequidditch/_primary.vhd
@@ -3,6 +3,14 @@ use verilog.vl_types.all;
 entity fakequidditch is
     port(
         clk             : in     vl_logic;
-        clk_out         : out    vl_logic
+        team1_vu_button : in     vl_logic;
+        team1_vd_button : in     vl_logic;
+        team2_vu_button : in     vl_logic;
+        team2_vd_button : in     vl_logic;
+        red             : out    vl_logic_vector(7 downto 0);
+        green           : out    vl_logic_vector(7 downto 0);
+        blue            : out    vl_logic_vector(7 downto 0);
+        hor_sync        : out    vl_logic;
+        ver_sync        : out    vl_logic
     );
 end fakequidditch;
